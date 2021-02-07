6ae60e4ce5eb8ccf9c2c29d2fb79293a55dc3f93
nashley2 <nashley2@cub.uca.edu>
2021-02-07 12:17:05 -0600

    finishing lab

--- main.cpp
+++ main.cpp
@@ -25,7 +25,19 @@ int main()
 	ll->erase(3);
 	ll->display();
 	// #3, Add 5 inserts
+	ll->insert(20, 2);
+	ll->insert(70, 5);
+	ll->insert(100, 0);
+	ll->insert(10, 5);
+	ll->insert(110, 6);
+	display()
 	// #6, Add 5 erases
+	ll->erase(3); 
+	ll->erase(5); 
+	ll->erase(4); 
+	ll->erase(2); 
+	ll->erase(1);
+	display()
 	delete ll;
 
 	cin.get();
